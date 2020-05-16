6c6a97663d275424e0dbee53f5ca3172503a1b0f
Gabriel Petersson <gabriielpetersson@gmail.com>
2020-05-16 18:28:55 +0200

    stuff

---
+++ src/components/login_page/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import InputField from './input_field/InputField';
+
+
+function Login() {
+  return (
+    <div className="container">
+      <InputField />
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/components/login_page/input_field/InputField.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './InputField';
+
+const InputField = () => (
+    <div>
+      <input type="text" />
+    </div>
+);
+
+export default InputField;
