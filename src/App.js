fff0f38285489096151281ff53f89f9e299690ed
gustafdb <99gustaf@gmail.com>
2020-05-16 19:26:11 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Nav from './components/nav/Nav';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div class="app">
-      <Nav />
+    <div className="app">
+      <Router>
+        <Nav />
+        <Switch>
+          <Route path="/"></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
