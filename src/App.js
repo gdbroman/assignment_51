085a1fb6e34d97668d92350b7e2f7c81ec247876
gustafdb <99gustaf@gmail.com>
2020-05-16 17:26:14 +0200

    structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import Nav from 'components/nav/nav';
 
 function App() {
   return (
     <div>
-
+      <Nav />
     </div>
   );
 }
---
+++ src/components/nav/nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './nav.scss';
+
+function Nav() {
+  return (
+    <nav>
+        
+    </nav>
+  );
+}
+
+export default Nav;
