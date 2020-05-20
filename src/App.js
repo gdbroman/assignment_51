bd2d6b016dada0c1be67b6b643842c77dc271bfa
gustafdb <99gustaf@gmail.com>
2020-05-20 18:05:00 +0200

    page templates

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import './App.scss';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar';
 import Login from './components/login_page/LoginScreen';
+//Pages
+import Location_page from './components/location_page/Location';
+import Trackers_page from './components/trackers_page/TrackerScreen';
+import Fences_page from './components/fences_page/Fences';
+import Journeys_page from './components/journeys_page/Journeys';
+import Events_page from './components/events_page/Events';
+import Users_page from './components/users_page/Users';
+import Profile_page from './components/profile_page/Profile';
+import Support_page from './components/support_page/Support';
+import Logout_page from './components/logout_page/Logout';
 
 class App extends React.Component {
   constructor(props) {
@@ -44,17 +54,15 @@ class App extends React.Component {
               <section className="main">
                 <Nav />
                 <section className="content">
-                  <Route path="/location">
-                    <h1>Location</h1>
-                  </Route>
-                  <Route path="/trackers" />
-                  <Route path="/fences" />
-                  <Route path="/journeys" />
-                  <Route path="/events" />
-                  <Route path="/users" />
-                  <Route path="/profile" />
-                  <Route path="/support" />
-                  <Route path="/logout" />
+                  <Route path="/location" component={Location_page} />
+                  <Route path="/trackers" component={Trackers_page} />
+                  <Route path="/fences" component={Fences_page} />
+                  <Route path="/journeys" component={Journeys_page} />
+                  <Route path="/events" component={Events_page} />
+                  <Route path="/users" component={Users_page} />
+                  <Route path="/profile" component={Profile_page} />
+                  <Route path="/support" component={Support_page} />
+                  <Route path="/logout" component={Logout_page} />
                 </section>
               </section>
             </Route>
