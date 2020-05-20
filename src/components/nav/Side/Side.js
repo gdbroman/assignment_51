500ff329b6684a2d47f358120f137b9333c8d220
gustafdb <99gustaf@gmail.com>
2020-05-20 21:15:01 +0200

    sidenav hide on click

--- src/components/nav/Side/Side.js
+++ src/components/nav/Side/Side.js
@@ -21,34 +21,34 @@ class Side extends React.Component {
                 <nav className="side">
                     <ul>
                     <NavLink to ="/location" exact={true} activeClassName='active'>
-                        <li><i className="material-icons">map</i><p>Location</p></li>
+                        <li onClick={this.slide}><i className="material-icons">map</i><p>Location</p></li>
                     </NavLink>
                     <NavLink to ="/trackers">
-                        <li><i className="material-icons">list</i><p>Possio Trackers</p></li>
+                        <li onClick={this.slide}><i className="material-icons">list</i><p>Possio Trackers</p></li>
                     </NavLink>
                     <NavLink to ="/fences">
-                        <li><i className="material-icons">tab_unselected</i><p>Geo-fences</p></li>
+                        <li onClick={this.slide}><i className="material-icons">tab_unselected</i><p>Geo-fences</p></li>
                     </NavLink>
                     <NavLink to ="/journeys">
-                        <li><i className="material-icons">device_hub</i><p>Journeys</p></li>
+                        <li onClick={this.slide}><i className="material-icons">device_hub</i><p>Journeys</p></li>
                     </NavLink>
                     <NavLink to ="/events"><div className="bb">
-                        <li><i className="material-icons">notifications</i><p>Events</p></li></div>
+                        <li onClick={this.slide}><i className="material-icons">notifications</i><p>Events</p></li></div>
                     </NavLink>
                     <NavLink to ="/users">
-                        <li><i className="material-icons">people</i><p>Users</p></li>
+                        <li onClick={this.slide}><i className="material-icons">people</i><p>Users</p></li>
                     </NavLink>
                     <NavLink to ="/profile">
-                        <li><i className="material-icons">settings</i><p>Profile</p></li>
+                        <li onClick={this.slide}><i className="material-icons">settings</i><p>Profile</p></li>
                     </NavLink>
                     <NavLink to ="/support">
-                        <div className="bb"><li><i className="material-icons">public</i><p>Support</p></li></div>
+                        <div className="bb"><li onClick={this.slide}><i className="material-icons">public</i><p>Support</p></li></div>
                     </NavLink>
                     <NavLink to ="/logout">
-                        <li><i className="material-icons">exit_to_app</i><p>Log out</p></li>
+                        <li onClick={this.slide}><i className="material-icons">exit_to_app</i><p>Log out</p></li>
                     </NavLink>
                     <NavLink to ="/login">
-                        <li><i className="material-icons">people</i><p>Log in</p></li>
+                        <li onClick={this.slide}><i className="material-icons">people</i><p>Log in</p></li>
                     </NavLink>
                     </ul>
                 </nav>
