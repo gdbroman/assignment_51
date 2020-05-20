8fee3471f6e60d89041881708c33d8490c034550
Gabriel Petersson <gabriielpetersson@gmail.com>
2020-05-20 17:09:31 +0200

    add tracker container

---
+++ src/components/trackers_page/tracker/Tracker.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Tracker.scss'
+
+
+class Tracker extends React.Component {
+  render() { 
+    return (  
+      <React.Fragment>
+        <p>teeeeeeeeeeeeeeeeeeeeeeeeeee</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Tracker
