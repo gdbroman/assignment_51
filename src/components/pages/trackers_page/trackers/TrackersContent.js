910877af605470e72aea626308cb7052fa492fee
Gabriel Petersson <gabriielpetersson@gmail.com>
2020-05-21 16:43:29 +0200

    styling

--- src/components/pages/trackers_page/trackers/TrackersContent.js
+++ src/components/pages/trackers_page/trackers/TrackersContent.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import StatusButton from '../status_button/StatusButton' 
+import Vehicle from '../../../../components/vehicle/Vehicle'
 
 import './TrackersContent.scss'
-import StatusButton from '../status_button/StatusButton' 
 
 
 class TrackersContent extends React.Component {
   render() { 
     return (  
-      <div className="status-button-container">
-        <StatusButton device_status="Active" />
-        <StatusButton device_status="Disabled" />
-      </div>
+      <React.Fragment>  
+        <div className="status-button-container">
+          <StatusButton status_link="active" status_title="Active" />
+          <StatusButton status_link="disabled" status_title="Disabled" />
+        </div>
+        <div className="trackers-vehicle-container">
+          <Vehicle />
+        </div>
+     </React.Fragment>
+      
     );
   }
 }
