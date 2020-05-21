f5ed889684c39123e706d7263d4539bd9d82e2e3
gustafdb <99gustaf@gmail.com>
2020-05-21 15:21:45 +0200

    vehicle

---
+++ src/components/vehicle/Vehicle.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Vehicle.scss';
+
+class Tab_page extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            type: "boat",
+            name: "Bogserbåt Grimmen",
+            events: {date: "21.05.2020 14:58", description: "Rörde sig"},
+        }
+    }
+
+    getSymbol() {
+        const type = this.state.type;
+        if (type === "boat") {
+            return "directions_boat";
+        }
+        else if (type === "car") {
+            return "directions_car";
+        }
+        else if (type === "bike") {
+            return "two_wheeler";
+        }
+        else {
+            return "gps_fixed";
+        }
+    }
+
+    render() {
+        return (
+            <div className="vehicle">
+                <span className="material-icons">{this.getSymbol()}</span>
+                <div className="text">
+                    <p className="name">{this.state.name}</p>
+                    <p className="date">{this.state.events['date']}</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Tab_page;
