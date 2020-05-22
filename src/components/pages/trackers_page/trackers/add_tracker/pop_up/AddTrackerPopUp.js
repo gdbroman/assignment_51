6f606ac810aea6fc34afbb42702e3da3a2d6103f
Gabriel Petersson <gabriielpetersson@gmail.com>
2020-05-22 19:07:45 +0200

    style popup

--- src/components/pages/trackers_page/trackers/add_tracker/pop_up/AddTrackerPopUp.js
+++ src/components/pages/trackers_page/trackers/add_tracker/pop_up/AddTrackerPopUp.js
@@ -3,6 +3,7 @@ import './AddTrackerPopUp.scss';
 
 const AddTrackerPopUp = () => (
     <div className="add-tracker-pop-up-container">
+        <i className="material-icons serial-back-arrow">clear</i>
         <div className="add-serial-container">
             <p className="serial-number-information">To add your device, enter the serial number. This information is available on your device and packaging.</p>
             <input className="serial-number-input" type="text" placeholder="Enter serial number" />
