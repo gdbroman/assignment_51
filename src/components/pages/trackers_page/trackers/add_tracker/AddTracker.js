2e6706dc17e8031bfbc3a46785bd7599065ab229
Gabriel Petersson <gabriielpetersson@gmail.com>
2020-05-22 16:00:16 +0200

    trcker

---
+++ src/components/pages/trackers_page/trackers/add_tracker/AddTracker.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './AddTracker.scss'
+
+const AddTracker = () => (
+    <div className="add-tracker-button">
+        <i className="material-icons add-tracker-icon">exposure_plus_1</i>
+    </div>
+
+)
+
+export default AddTracker;
