4c6270dc3277b309d1b96e564241039d09dfbe07
Gabriel Petersson <gabriielpetersson@gmail.com>
2020-05-23 19:17:58 +0200

    add style to journeys

--- src/components/pages/journeys_page/Journeys.js
+++ src/components/pages/journeys_page/Journeys.js
@@ -10,7 +10,34 @@ const journeys = [{
   detailed_to: 'Stad 2 klippan',
   date_from: '2017',
   date_to: '2018-03',
-  vehicle_name: 'Boat',
+  vehicle_name: 'Burke the boat',
+  journey_reason: 'Business',
+},{
+  short_from:'Stad 1', 
+  short_to: 'Stad 2',
+  detailed_from: 'Stad 1 Märket',
+  detailed_to: 'Stad 2 klippan',
+  date_from: '2017',
+  date_to: '2018-03',
+  vehicle_name: 'Båt Tor',
+  journey_reason: 'Business',
+},{
+  short_from:'Stad 1', 
+  short_to: 'Stad 2',
+  detailed_from: 'Stad 1 Märket',
+  detailed_to: 'Stad 2 klippan',
+  date_from: '2017',
+  date_to: '2018-03',
+  vehicle_name: 'Frej',
+  journey_reason: 'Business',
+},{
+  short_from:'Stad 1', 
+  short_to: 'Stad 2',
+  detailed_from: 'Stad 1 Märket',
+  detailed_to: 'Stad 2 klippan',
+  date_from: '2017',
+  date_to: '2018-03',
+  vehicle_name: 'Båte',
   journey_reason: 'Business',
 }]
 
@@ -21,7 +48,7 @@ class JourneysPage extends React.Component {
 
   render() {
     return (
-      <div className="location_page">
+      <div className="">
           {journeys.map(journey => <Journey journey={journey} />)}
       </div>
     );
--- src/components/pages/journeys_page/journey/Journey.js
+++ src/components/pages/journeys_page/journey/Journey.js
@@ -1,21 +1,25 @@
 import React from 'react';
-
+import './Journey.scss';
 
 const Route = ({detailed_address, date}) => (
     <div className="route-container">
-        <p>{detailed_address}</p>
-        <p className="">{date}</p>
+        <p className="route-text">{detailed_address}</p>
+        <p className="route-text">{date}</p>
     </div>
 )
 
 
 const Journey = ({journey: {short_from, short_to, detailed_from, detailed_to, date_from, date_to, vehicle_name, journey_reason}}) => (
         <div className="journey-container">
-            <p>{short_from} - {short_to}</p>
-            <p className="journey-reason">{journey_reason}</p>
-            <div className="journey-vehicle-container">
-                <span className="material-icons journey-vehicle-icon">directions_boat</span>
-                <p>{vehicle_name}</p>
+            <div className="journey-vehicle-info">
+                <div className="journey-icon-container">
+                    <p className="journey-reason">{journey_reason}</p>
+                    <span className="material-icons journey-vehicle-icon">directions_boat</span>
+                </div>
+                <div className="journey-info">
+                    <p className="journey-destinations">{short_from} - {short_to}</p>
+                    <p className="journey-vehicle-name">{vehicle_name}</p>
+                </div>
             </div>
             <div className="journey-route-container">
                 <Route detailed_address={detailed_from} date={date_from}/>
