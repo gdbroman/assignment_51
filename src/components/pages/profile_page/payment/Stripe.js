c5bcebf8d0cd9c97b7d3978880c65d120afb4eb3
gustafdb <99gustaf@gmail.com>
2020-05-25 19:08:02 +0200

    profile

---
+++ src/components/pages/profile_page/payment/Stripe.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {loadStripe} from '@stripe/stripe-js';
+import {CardElement, Elements, ElementsConsumer} from '@stripe/react-stripe-js';
+import './Stripe.scss';
+
+class CheckoutForm extends React.Component {
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const {stripe, elements} = this.props;
+    const {error, paymentMethod} = await stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardElement),
+    });
+  };
+
+  render() {
+    const {stripe} = this.props;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <CardElement />
+        <button type="submit" id="submit" disabled={!stripe}>
+          Pay
+        </button>
+      </form>
+    );
+  }
+}
+
+const InjectedCheckoutForm = () => (
+  <ElementsConsumer>
+    {({stripe, elements}) => (
+      <CheckoutForm stripe={stripe} elements={elements} />
+    )}
+  </ElementsConsumer>
+);
+
+const stripePromise = loadStripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
+
+const Pay = () => (
+  <Elements stripe={stripePromise}>
+    <InjectedCheckoutForm />
+  </Elements>
+);
+
+export default Pay;
